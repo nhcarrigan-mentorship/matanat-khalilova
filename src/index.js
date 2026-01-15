5b060c8c26bf90edc0450e1a34135592afa09059
matanatkhalil <khalilmatanat@gmail.com>
2026-01-15 14:09:44 +0400

    style: fix formatting issues with prettier

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import SignupForm from './components/SignupForm';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import SignupForm from "./components/SignupForm";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <SignupForm />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
