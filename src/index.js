c0b844469bae2bc28e32d62bf1fb157ee6af6633
matanatkhalil <khalilmatanat@gmail.com>
2026-01-15 14:03:49 +0400

    feat: add required project structure to resolve build errors

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import SignupForm from './components/SignupForm';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <SignupForm />
+  </React.StrictMode>
+);
