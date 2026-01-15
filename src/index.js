ccd4c3da7c4342a2023ca789d37e01447b94be2a
matanatkhalil <khalilmatanat@gmail.com>
2026-01-15 19:09:21 +0400

    fix: add file extension to SignupForm import for ESM compatibility

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import SignupForm from "./components/SignupForm";
+import SignupForm from './components/SignupForm.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
